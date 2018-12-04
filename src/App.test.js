dd26cdd94ba7546264a80681ec0405b71d1ef3cf
James Sutherland <jrsutherland78@googlemail.com>
2018-12-04 21:49:49 +0000

    jest/emzyme setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./setupTests"
+import { shallow, mount, render } from 'enzyme';
+
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders welcome message', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.contains(<p>Welcome</p>)).toEqual(true)
+});
+
+it('renders check', () => {
+  shallow(<App />);
 });
