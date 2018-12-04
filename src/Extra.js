d6d1b2a7ce75ec9106f183ef3614e46192766069
James Sutherland <jrsutherland78@googlemail.com>
2018-12-04 18:34:29 +0000

    Extra changed to functional component

--- src/Extra.js
+++ src/Extra.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Extra extends Component{
-  render() {
-    // console.log(this.props)
-    const { extras } = this.props
-    const extraList = extras.map(extra => {
-      return (
-        <div className="peep" key={ extra.id }>
-          <div>Name: { extra.name }</div>
-          <div>Age: { extra.age }</div>
-          <div>Interests: { extra.interests }</div>
-        </div>
-      )
-    })
-
+const Extra = (props) => {
+  // console.log(this.props)
+  const { extras } = props
+  const extraList = extras.map(extra => {
     return (
-      <div className="extra-list">
-        { extraList }
+      <div className="peep" key={ extra.id }>
+        <div>Name: { extra.name }</div>
+        <div>Age: { extra.age }</div>
+        <div>Interests: { extra.interests }</div>
       </div>
     )
-  }
+  })
+
+  return (
+    <div className="extra-list">
+      { extraList }
+    </div>
+  )
 }
 
 export default Extra
