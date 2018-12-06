3b6d3fe53abafb0eb3c948c0449e3b58b23e5da4
James Sutherland <jrsutherland78@googlemail.com>
2018-12-06 11:21:50 +0000

    render text that there are no todos

--- __tests__/Todos.test.js
+++ __tests__/Todos.test.js
@@ -6,12 +6,19 @@ import { shallow, mount, render } from 'enzyme';
 import Todos from '../src/Todos';
 
 describe("Todos", () => {
-  it("Todos should render properly",() => {
+  it("Todos should render todo list",() => {
     const props = {todos : [
-      { id: 1, content:'buy some milk'},
+      { id: 1, content:'buy some milk'}
     ]};
     const wrapper = mount(<Todos {...props} />)
 
     expect(wrapper.contains("buy some milk")).toEqual(true)
+  });
+
+  it("Todos should render no todo list",() => {
+    const props = {todos : []};
+    const wrapper = mount(<Todos {...props} />)
+
+    expect(wrapper.contains("You have no todos")).toEqual(true)
   });
 });
