cac42dc6850237fefbe98086da4c89cc6e53ecb8
James Sutherland <jrsutherland78@googlemail.com>
2018-12-06 22:04:49 +0000

    add cursor default on listed todos

--- src/Todos.js
+++ src/Todos.js
@@ -6,7 +6,7 @@ const Todos = ({todos, deleteTodo}) => {
     todos.map(todo => {
       return (
         <div className="collection-item" key={todo.id}>
-          <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
+          <span style={{ cursor: 'default' }} onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
         </div>
       )
     })
